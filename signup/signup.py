92f924535b9d507749edbcdef82a597de5c77aba
breakfastpasta <breakfastpasta@protonmail.com>
2024-09-18 13:33:11 -0700

    remove unpacking args

--- signup/signup.py
+++ signup/signup.py
@@ -47,7 +47,7 @@ class SignUp(commands.Cog):
     @app_commands.command()
     @app_commands.describe(team_name="Your team's name")
     @app_commands.describe(players="List of player mentions")
-    async def signup(self, interaction: discord.Interaction, team_name: str, *players: discord.Member):
+    async def signup(self, interaction: discord.Interaction, team_name: str, players: str):
         guild_group = self.config.guild(ctx.guild)
         async with guild_group.current_teams() as current_teams:
             current_teams[team_name] = players
